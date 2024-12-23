c9ac6ce1a2bfa495e74341b0e0de927f6b057ed6
output <79727016+UntitledOutput@users.noreply.github.com>
2024-12-23 12:00:44 -0500

    Auto commit at

--- include/tomato/Audio/audio.cpp
+++ include/tomato/Audio/audio.cpp
@@ -4,14 +4,19 @@
 
 #include "audio.hpp"
 
-
 using namespace tmt::audio;
+
+#define CHECK_RESULT(result)                                                                                           \
+    if (result != MA_SUCCESS)                                                                                          \
+    {                                                                                                                  \
+        printf("Error: %s\n", ma_result_description(result));                                                          \
+        return;                                                                                                   \
+    }
+
 
 tmt::audio::AudioDevice* AudioDevice::mInstance;
 
 ma_engine engine;
-ma_device device;
-ma_context context;
 ma_resource_manager resource_manager;
 
 ma_sound_group defaultGroup;
@@ -42,86 +47,30 @@ tmt::audio::AudioDevice::AudioDevice()
 {
     mInstance = this;
     ma_result result;
-    ma_device_info* pPlaybackDeviceInfos;
-    u32 playbackDeviceCount;
 
-    var resource_mgr_cfg = ma_resource_manager_config_init();
-    resource_mgr_cfg.decodedFormat = ma_format_f32;
-    resource_mgr_cfg.decodedChannels = 0;
-    resource_mgr_cfg.decodedSampleRate = 48000;
+    ma_engine_config pConfig;
+    pConfig.channels = 2;
+    pConfig.sampleRate = 48000;
 
-    result = ma_resource_manager_init(&resource_mgr_cfg, &resource_manager);
+    result = ma_engine_init(NULL, &engine);
     if (result != MA_SUCCESS)
     {
         return;
     }
-    
-    /* We're going to want a context so we can enumerate our playback devices. */
-    result = ma_context_init(NULL, 0, NULL, &context);
+    ma_sound sound;
+    result = ma_sound_init_from_file(&engine, "resources/sound/call.wav", 0, nullptr, nullptr, &sound);
     if (result != MA_SUCCESS)
     {
+        //printf("WARNING: Failed to load sound \"%s\"", path.c_str());
         return;
     }
-
-    result = ma_context_get_devices(&context, &pPlaybackDeviceInfos, &playbackDeviceCount, NULL, NULL);
-    if (result != MA_SUCCESS)
-    {
-        ma_context_uninit(&context);
-        return;
-    }
-    int deviceId = 0;
-    for (int i = 0; i < playbackDeviceCount; ++i)
-    {
-        printf("%d: %s\n", i, pPlaybackDeviceInfos[i].name);
-        if (pPlaybackDeviceInfos[i].isDefault)
-        {
-            deviceId = i;
-            break;
-        }
-    }
-
-    ma_device_config deviceConfig = ma_device_config_init(ma_device_type_playback);
-    deviceConfig.playback.format = resource_manager.config.decodedFormat;
-    deviceConfig.playback.channels = 0;
-    deviceConfig.playback.pDeviceID = &pPlaybackDeviceInfos[deviceId].id;
-    deviceConfig.sampleRate = resource_manager.config.decodedSampleRate;
-    deviceConfig.pUserData = &engine;
-    deviceConfig.dataCallback = data_callback;
-
-    result = ma_device_init(&context, &deviceConfig, &device);
-    if (result != MA_SUCCESS)
-    {
-        return;
-    }
-
-    var pConfig = ma_engine_config_init();
-    pConfig.pDevice = &device;
-    pConfig.pResourceManager = &resource_manager;
-    //pConfig.noAutoStart = MA_TRUE;
-    pConfig.listenerCount = 1;
-
-    ma_log log;
-    result = ma_log_init(NULL, &log);
-    ma_log_register_callback(&log, ma_log_callback_init(log_callback, nullptr));
-
-    pConfig.pLog = &log;
-
-    result = ma_engine_init(&pConfig, &engine);
-    if (result != MA_SUCCESS)
-    {
-        return;
-    }
-
-    ma_engine_play_sound(&engine, "resources/sound/call.wav", NULL);
-
+    ma_sound_start(&sound);
 }
 
 AudioDevice::~AudioDevice()
 {
     ma_engine_uninit(&engine);
-    ma_device_uninit(&device);
-    ma_context_uninit(&context);
-    ma_resource_manager_uninit(&resource_manager);
+    //ma_resource_manager_uninit(&resource_manager);
 }
 
 void AudioDevice::Update()
@@ -149,16 +98,16 @@ void AudioDevice::AddListener(SoundListener* listener)
 tmt::audio::Sound::Sound(string path)
 {
     ma_sound sound;
-    var result = ma_sound_init_from_file(
-        &engine, path.c_str(), MA_RESOURCE_MANAGER_DATA_SOURCE_FLAG_DECODE | MA_RESOURCE_MANAGER_DATA_SOURCE_FLAG_ASYNC,
-        NULL, NULL, &sound);
+    var result = ma_sound_init_from_file(&engine, path.c_str(), 0,
+        nullptr, nullptr, &sound);
     if (result != MA_SUCCESS)
     {
-        printf("WARNING: Failed to load sound \"%s\"", path);
+        printf("WARNING: Failed to load sound \"%s\"", path.c_str());
         return;
     }
+    ma_sound_start(&sound);
 
-    ma_sound_set_spatialization_enabled(&sound, true);
+    //ma_sound_set_spatialization_enabled(&sound, true);
 
     pId = sounds.size();
     sounds.push_back(sound);
