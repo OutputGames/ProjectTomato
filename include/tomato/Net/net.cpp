3b660554d8b775510d6c8bf197a737233e018c0d
output <79727016+UntitledOutput@users.noreply.github.com>
2025-01-01 23:00:41 -0500

    Auto commit at

--- include/tomato/Net/net.cpp
+++ include/tomato/Net/net.cpp
@@ -1,4 +1,35 @@
 #include "net.hpp"
 
+#include <cstdio>
+#include <enet/enet.h>
 
-void tmt::net::init() {}
+ENetAddress address;
+ENetHost* server;
+
+void tmt::net::init()
+{
+    if (enet_initialize() != 0)
+    {
+        fprintf(stderr, "An error occurred while initializing ENet.\n");
+        return;
+    }
+
+    atexit(enet_deinitialize);
+
+    address.host = ENET_HOST_ANY;
+    address.port = 1234;
+
+    server = enet_host_create(&address, 32, 2, 0, 0);
+
+    if (server == nullptr)
+    {
+        fprintf(stderr, "An error occurred while trying to create an ENet server host.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    atexit([]()
+    {
+        enet_host_destroy(server);
+    });
+
+}
