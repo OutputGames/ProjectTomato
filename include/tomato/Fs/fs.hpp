9367471bd299abf5a246585c31d098f542a9460b
output <79727016+UntitledOutput@users.noreply.github.com>
2025-01-08 12:00:56 -0500

    Auto commit at

--- include/tomato/Fs/fs.hpp
+++ include/tomato/Fs/fs.hpp
@@ -3,6 +3,8 @@
 
 #include "utils.hpp"
 
+#include <sstream>
+
 namespace tmt::render
 {
     struct SceneDescription;
@@ -97,6 +99,223 @@ namespace tmt::fs
 
     };
 
+    struct BinaryDataReader
+    {
+        enum ByteOrder
+        {
+            BigEndian,
+            LittleEndian
+        } byteOrder = LittleEndian;
+
+        // BinaryReader(std::string path);
+
+        template <class T>
+        void endswap(T* objp)
+        {
+            auto memp = reinterpret_cast<unsigned char*>(objp);
+            std::reverse(memp, memp + sizeof(T));
+        }
+
+        template <typename T>
+        T Read()
+        {
+            T value;
+            std::memcpy(&value, data + position_, sizeof(T));
+            position_ += sizeof(T);
+            return value;
+        }
+
+        template <typename T>
+        T* ReadArray(int count)
+        {
+            T* t = new T[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                t[i] = Read<T>();
+            }
+
+            return t;
+        }
+
+        template <typename T>
+        std::vector<T> ReadArray(ulong offset, int count)
+        {
+            long p = tellg();
+
+            SeekBegin(offset);
+            var list = ReadArray<T>(count);
+
+            SeekBegin(p);
+            return list;
+        }
+
+        bool CheckSignature(string sig)
+        {
+            var s = ReadString(sig.size());
+            return s == sig;
+        }
+
+        glm::vec3 ReadVec3()
+        {
+            float x = ReadSingle();
+            float y = ReadSingle();
+            float z = ReadSingle();
+            var vec = glm::vec3(x, y, z);
+            return vec;
+        }
+
+        glm::vec4 ReadVec4()
+        {
+            float x = ReadSingle();
+            float y = ReadSingle();
+            float z = ReadSingle();
+            float w = ReadSingle();
+            var vec = glm::vec4(x, y, z, w);
+            return vec;
+        }
+
+        glm::vec2 ReadVec2() { return glm::vec2(ReadSingle(), ReadSingle()); }
+
+        glm::quat ReadQuat()
+        {
+            var v = ReadVec4();
+
+            return glm::quat(v.w, v.x, v.y, v.z);
+        }
+
+        u16* ReadUInt16Array(int size)
+        {
+            var arr = new u16[size];
+
+            for (int i = 0; i < size; ++i)
+            {
+                arr[size] = ReadUInt16();
+            }
+
+            return arr;
+        }
+
+        u8* ReadByteArray(int size)
+        {
+            var arr = new u8[size];
+
+            for (int i = 0; i < size; ++i)
+            {
+                arr[size] = ReadByte();
+            }
+
+            return arr;
+        }
+
+
+        void Skip(int count) { seekg(count, std::ios::_Seekcur); }
+
+        float ReadSingle() { return Read<float>(); }
+
+
+        float* ReadFloatArray(int ct)
+        {
+            var arr = new float[ct];
+
+            for (int i = 0; i < ct; ++i)
+            {
+                arr[i] = ReadSingle();
+            }
+
+            return arr;
+        }
+
+        float ReadHalfFloat() { return ReadSingle(); }
+
+        s8 ReadSByte() { return Read<s8>(); }
+
+        size_t fileSize;
+
+        void SeekBegin(int pos) { seekg(pos, std::ios::_Seekbeg); }
+
+        void SeekCurrent(int pos) { seekg(pos, std::ios::_Seekcur); }
+
+        void Align(int alignment) { seekg((-tellg() % alignment + alignment) % alignment, std::ios::_Seekcur); }
+
+        BinaryDataReader(byte* data)
+        {
+            this->data = data;
+
+        }
+
+        u64 ReadUInt64() { return Read<u64>(); }
+
+        u32 ReadUInt32() { return Read<u32>(); }
+
+        u16 ReadUInt16() { return Read<u16>(); }
+
+        s16 ReadInt16() { return Read<s16>(); }
+
+        s32 ReadInt32() { return Read<s32>(); }
+
+        s64 ReadInt64() { return Read<s64>(); }
+
+        ByteOrder ReadByteOrder()
+        {
+            byteOrder = BigEndian;
+            var bom = Read<ByteOrder>();
+            byteOrder = bom;
+            return bom;
+        }
+
+        unsigned char ReadByte() { return Read<unsigned char>(); }
+
+        u32 ReadOffset()
+        {
+            var offset = static_cast<u32>(ReadUInt64());
+            return offset == 0 ? 0 : offset;
+        }
+
+        string ReadString(int size)
+        {
+            string s = "";
+
+            for (int i = 0; i < size; ++i)
+            {
+                s += Read<char>();
+            }
+
+            return s;
+        }
+
+        string ReadString()
+        {
+            var size = ReadInt32();
+            return ReadString(size);
+        }
+
+    private:
+        size_t tellg()
+        {
+            return position_;
+        }
+
+        void seekg(size_t position, std::ios_base::seekdir dir)
+        {
+            switch (dir)
+            {
+                case std::ios::_Seekbeg:
+                    position_ = position;
+                    break;
+                case std::ios::_Seekcur:
+                    position_ += position;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        const byte* data;
+        size_t size_;
+        size_t position_ = 0;
+    };
+
     struct BinaryReader : std::ifstream
     {
         enum ByteOrder
@@ -105,7 +324,7 @@ namespace tmt::fs
             LittleEndian
         } byteOrder = LittleEndian;
 
-        BinaryReader(std::string path);
+        //BinaryReader(std::string path);
 
         template <class T>
         void endswap(T* objp)
@@ -210,17 +429,6 @@ namespace tmt::fs
             return arr;
         }
 
-        u64 ReadUInt64();
-        u32 ReadUInt32();
-        u16 ReadUInt16();
-
-        s16 ReadInt16();
-        s32 ReadInt32();
-        s64 ReadInt64();
-
-
-        ByteOrder ReadByteOrder();
-        unsigned char ReadByte();
 
         void Skip(int count)
         {
@@ -255,10 +463,6 @@ namespace tmt::fs
             return Read<s8>();
         }
 
-        u32 ReadOffset();
-        string ReadString(int size);
-        string ReadString();
-
         size_t fileSize;
 
         void SeekBegin(int pos)
@@ -272,8 +476,56 @@ namespace tmt::fs
         {
             seekg((-tellg() % alignment + alignment) % alignment, _Seekcur);
         }
+
+        BinaryReader(std::string path) :
+            std::ifstream(path, std::ios::binary) { fileSize = tellg(); }
+
+        u64 ReadUInt64() { return Read<u64>(); }
+
+        u32 ReadUInt32() { return Read<u32>(); }
+
+        u16 ReadUInt16() { return Read<u16>(); }
+
+        s16 ReadInt16() { return Read<s16>(); }
+
+        s32 ReadInt32() { return Read<s32>(); }
+
+        s64 ReadInt64() { return Read<s64>(); }
+
+        ByteOrder ReadByteOrder()
+        {
+            byteOrder = BigEndian;
+            var bom = Read<ByteOrder>();
+            byteOrder = bom;
+            return bom;
+        }
+
+        unsigned char ReadByte() { return Read<unsigned char>(); }
+
+        u32 ReadOffset()
+        {
+            var offset = static_cast<u32>(ReadUInt64());
+            return offset == 0 ? 0 : offset;
+        }
+
+        string ReadString(int size)
+        {
+            string s = "";
+
+            for (int i = 0; i < size; ++i)
+            {
+                s += Read<char>();
+            }
+
+            return s;
+        }
+
+        string ReadString()
+        {
+            var size = ReadInt32();
+            return ReadString(size);
+        }
     };
-    ;
 
 #define RESFUNC(name, p1, p2, ret) private: \
         ret _##name##p1; \
